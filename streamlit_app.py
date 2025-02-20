b14f66096770c6e89000bbd6ec82acc1ac0222db
Meenal Sharma <119120576+MeenalMSharma@users.noreply.github.com>
2025-02-20 10:24:49 +0530

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("🎈 My new app")
+st.write(
+    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](http://127.0.0.1:5500/index.html)."
+)

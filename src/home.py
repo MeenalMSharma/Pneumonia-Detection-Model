daf17a06eb8bfa6c49dde46ec497c1ea1aab4d6b
Meenal Sharma <119120576+MeenalMSharma@users.noreply.github.com>
2025-03-02 23:00:42 +0530

    Update home.py

--- src/home.py
+++ src/home.py
@@ -2,11 +2,11 @@ import streamlit as st
 
 def main():
     st.markdown(
-        '''
+    
         <h1 align="center">
-            Pneumonia Detection Model
+            <b><u>Pneumonia Detection Model</u></b>
         </h1><br>
-        '''
+        
     )  # <-- Closing triple quotes added here
 
 if __name__ == '__main__':

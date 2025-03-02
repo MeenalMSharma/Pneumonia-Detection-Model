faabbdb1ea1494f183f763506051a476c39a0c42
Meenal Sharma <119120576+MeenalMSharma@users.noreply.github.com>
2025-03-02 10:56:59 +0530

    Update about1.py

--- src/about.py
+++ src/about.py
@@ -1,73 +1,62 @@
 import streamlit as st
 
-
 def main(): 
-  
-    st.markdown( 
-
-
-       
-        
+    st.markdown(
         '''
-        
-
         <h2 align="center">
             About Us
         </h2>
 
         ## The Team
-                
-
         ''', 
         unsafe_allow_html=True, 
-         )  
-    col1, mid, col2 = st.columns([20,1,9])
+    )  
+
+    col1, mid, col2 = st.columns([20, 1, 9])
+
     with col1:
-          st.markdown( 
-        '''
-        ### Anant Pratap Singh
-                
-        -  I'm currently learning about **Machine Learning**
-        ##### Reach me
-    
-        <h5 align="center">
-            <strong><a href="https://github.com/Warlord-K">Github</a></strong> |
-            <strong><a href="https://www.linkedin.com/in/yatharth-gupta-012177228/">LinkedIn</a></strong>
-        </h5> 
-        <br> 
-        <br>
-        ''', 
-        unsafe_allow_html=True,
-    )
-      with col2:
-         st.image("https://drive.google.com/file/d/1wQ_OG-5wnpDtPx4p0WFUwtcggb330ijl/view?usp=sharing", width = 260)
-    
-    
-         
-    col1, mid, col2 = st.columns([20,1,9])
-    with col1:
-          st.markdown( 
+        st.markdown(
             '''
-        ### Meenal Sharma
-        
-        -  I'm currently learning about **Machine Learning**
-         ##### Reach me
+            ### Anant Pratap Singh
+                
+            -  I'm currently learning about **Machine Learning**
+            ##### Reach me
+    
+            <h5 align="center">
+                <strong><a href="https://github.com/Warlord-K">Github</a></strong> |
+                <strong><a href="https://www.linkedin.com/in/yatharth-gupta-012177228/">LinkedIn</a></strong>
+            </h5> 
+            <br> 
+            <br>
+            ''', 
+            unsafe_allow_html=True,
+        )
 
-        <h5 align="center">
-           <strong><a href="https://github.com/surya1176">Github</a></strong> |
-            <strong><a href="https://www.linkedin.com/in/surya-karthikeya-327255228">LinkedIn</a></strong>
-        </h5>
-        <br> 
-        <br> 
-        ''', 
-        unsafe_allow_html=True,
-        )  
- 
     with col2:
-         st.image("https://drive.google.com/file/d/1wadp8dr8U-6c2VBBqNO3elltyDH1kmvh/view?usp=sharing", width = 260)
-          
-   
+        st.image("https://drive.google.com/uc?export=view&id=1wQ_OG-5wnpDtPx4p0WFUwtcggb330ijl", width=260)
 
+    col1, mid, col2 = st.columns([20, 1, 9])
+
+    with col1:
+        st.markdown(
+            '''
+            ### Meenal Sharma
+            
+            -  I'm currently learning about **Machine Learning**
+             ##### Reach me
+
+            <h5 align="center">
+                <strong><a href="https://github.com/surya1176">Github</a></strong> |
+                <strong><a href="https://www.linkedin.com/in/surya-karthikeya-327255228">LinkedIn</a></strong>
+            </h5>
+            <br> 
+            <br> 
+            ''', 
+            unsafe_allow_html=True,
+        )
+
+    with col2:
+        st.image("https://drive.google.com/uc?export=view&id=1wadp8dr8U-6c2VBBqNO3elltyDH1kmvh", width=260)
 
 if __name__ == '__main__':
     main()

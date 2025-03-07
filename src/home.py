f5330f6f26b62d03b5c039bd9149cb7ab3846f64
Meenal Sharma <119120576+MeenalMSharma@users.noreply.github.com>
2025-03-07 09:28:06 +0530

    home.py

--- src/home.py
+++ src/home.py
@@ -2,12 +2,13 @@ import streamlit as st
 
 def main():
     st.markdown(
-    
-        <h1 align="center">
+        '''
+        <h1 style="text-align:center;">
             <b><u>Pneumonia Detection Model</u></b>
-        </h1><br>
-        
-    )  # <-- Closing triple quotes added here
+        </h1>
+        ''', 
+        unsafe_allow_html=True  # Required to render HTML properly
+    )
 
 if __name__ == '__main__':
     main()

fbc4cd677a01b57186941412def879872a8c4e7c
Meenal Sharma <119120576+MeenalMSharma@users.noreply.github.com>
2025-03-07 09:29:30 +0530

    info.py

--- src/info.py
+++ src/info.py
@@ -1,31 +1,18 @@
 import streamlit as st
 
-
 def main(): 
-  
-    st.markdown ( 
-
-
-       
-        
+    st.markdown( 
         ''' 
-        <h1 align="center">About the Dataset</h1><br>
+        <h1 style="text-align:center;">About the Dataset</h1>
         <h2>What is Pneumonia?</h2>
-        Pneumonia is a lung infection that causes inflammation and fluid or pus in the air sacs of the lungs. It can be caused by bacteria, viruses, or fungi. 
-        <h2>About Dataset</h2> 
+        <p>Pneumonia is a lung infection that causes inflammation and fluid or pus in the air sacs of the lungs. 
+        It can be caused by bacteria, viruses, or fungi.</p>
         
-        This dataset contains 7022 images of human Lungs' X-Ray.
-        
-        <br>
-        
-
-
-        
-
+        <h2>About Dataset</h2>
+        <p>This dataset contains 7022 images of human lungs' X-ray.</p>
         ''',
         unsafe_allow_html=True,
     ) 
+
 if __name__ == '__main__':
     main()
-
- 

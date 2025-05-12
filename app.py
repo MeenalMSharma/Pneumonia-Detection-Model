81c3a1fd4573ff00cff8d5f8d73d63a0fd9437ae
Meenal Sharma <119120576+MeenalMSharma@users.noreply.github.com>
2025-05-12 09:43:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,44 +3,46 @@ from src import about, home, info, mail, pneumonia
 import os
 
 def init():
-st.session_state.page = 'Homepage'
-st.session_state.project = False
-st.session_state.model = False
+    
+    st.session_state.page = 'Homepage'
+    st.session_state.project = False
+    st.session_state.model = False
 
 ```
-st.session_state.pages = {
-    'Homepage': home.main,
-    'Pneumonia Detection': pneumonia.main,
-    'About the Dataset': info.main,
-    'About Us': about.main,
-    'Message Us': mail.main
-}
+    st.session_state.pages = {
+        'Homepage': home.main,
+        'Pneumonia Detection': pneumonia.main,
+        'About the Dataset': info.main,
+        'About Us': about.main,
+        'Message Us': mail.main
+    }
 ```
 
 def draw_style():
-style = """ <style>
-.stApp {
-background-image: url('');  /\* Optional: Add your image URL or leave blank \*/
-background-size: cover;
-background-repeat: no-repeat;
-background-position: center;
-}
-header {visibility: visible;}
-footer {visibility: hidden;} </style>
+    style = """
+    <style>
+    .stApp {
+    background-image: url('');  /\* Optional: Add your image URL or leave blank \*/
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    }
+    header {visibility: visible;}
+    footer {visibility: hidden;} </style>
 """
-st.markdown(style, unsafe_allow_html=True)
-
+    st.markdown(style, unsafe_allow_html=True)
+    
 def load_page():
-if st.session_state.page in st.session_state.pages:
-st.session_state.pages[st.session_state.page]()
-else:
-st.warning("The selected page does not exist!")
+    if st.session_state.page in st.session_state.pages:
+        st.session_state.pages[st.session_state.page]()
+    else:
+        st.warning("The selected page does not exist!")
 
 def set_page(loc=None, reset=False):
-if st.session_state.page != 'Homepage':
-for key in list(st.session_state.keys()):
-if key not in ('page', 'project', 'model', 'pages'):
-st.session_state.pop(key)
+    if st.session_state.page != 'Homepage':
+        for key in list(st.session_state.keys()):
+            if key not in ('page', 'project', 'model', 'pages'):
+                st.session_state.pop(key)
 
 ```
 st.session_state.page = loc if loc else st.session_state.get('set', 'Homepage')
@@ -53,12 +55,12 @@ elif st.session_state.page in ('Message Us', 'About Us'):
 ```
 
 def change_button():
-set_page('Pneumonia Detection')
-st.session_state.model = True
-st.session_state.project = True
+    set_page('Pneumonia Detection')
+    st.session_state.model = True
+    st.session_state.project = True
 
 def main():
-st.set_page_config(page_title='Pneumonia Detection')
+    st.set_page_config(page_title='Pneumonia Detection')
 
 ```
 if 'page' not in st.session_state:
@@ -68,7 +70,7 @@ draw_style()
 
 with st.sidebar:
     project, about_btn, contact = st.columns([0.8, 1, 1.2])
-
+    
     if not st.session_state.project:
         project.button('Models', on_click=change_button)
     else:
@@ -76,12 +78,12 @@ with st.sidebar:
 ```
 
 if st.session_state.project and st.session_state.model:
-selected_model = st.radio(
-'Models',
-['Pneumonia Detection', 'Brain Tumor Detection'],  # Add Brain Tumor as an option
-key='set',
-on_change=set_page,
-)
+    selected_model = st.radio(
+        'Models',
+        ['Pneumonia Detection', 'Brain Tumor Detection'],  # Add Brain Tumor as an option
+        key='set',
+        on_change=set_page,
+    )
 
 ```
     about_btn.button('About Us', on_click=set_page, args=('About Us',))

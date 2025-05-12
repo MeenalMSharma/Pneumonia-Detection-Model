9c249377f7d07dfa7844b5135482ee0f2e06a61a
Meenal Sharma <119120576+MeenalMSharma@users.noreply.github.com>
2025-05-12 06:50:40 +0530

    Update info.py

--- src/info.py
+++ src/info.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-def main(): 
-    st.markdown( 
-        ''' 
+def main():
+    st.markdown(
+        '''
         <h1 style="text-align:center;">About the Dataset</h1>
         <h2>What is Pneumonia?</h2>
         <p>Pneumonia is a lung infection that causes inflammation and fluid or pus in the air sacs of the lungs. 
@@ -11,7 +11,7 @@ def main():
         <h2>About Dataset</h2>
         <p>This dataset contains 7022 images of human lungs' X-ray.</p>
         ''',
-        unsafe_allow_html=True,
+        unsafe_allow_html=True,  # Required to render HTML properly
     ) 
 
 if __name__ == '__main__':
